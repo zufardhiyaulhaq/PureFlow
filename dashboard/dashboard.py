e9e566b4e4c7f35909d2949af67462062b1eaff3
zufardhiyaulhaq <zufar@ieee.org>
2018-07-26 10:26:54 +0700

    testingv2

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -26,14 +26,9 @@ def openvswitch_api():
     raw = request.form.to_dict(flat=True)
     data = {k.encode('utf8'): v.encode('utf8') for k, v in raw.items()}
 
-    print (data)
-    print (type(data))
-    print (data["device-type"])
-    print (type(data["device-type"]))
-    # if data["device-type"] == "openvswitch":
-    #     os.system('ansible-playbook -u %s -i %s, /opt/PureFlow/ansible/playbook/openvswitch/openvswitch.yaml --extra-vars "controller=%s bridge=%s"'%(data["username"],data["device-ip"],data["controller"],data["bridge"]))
-    #     return "success!"
-    return "success!"
+    if data["device-type"] == "openvswitch":
+        os.system('ansible-playbook -u %s -i %s, /opt/PureFlow/ansible/playbook/openvswitch/openvswitch.yaml --extra-vars "controller=%s bridge=%s"'%(data["username"],data["device-ip"],data["controller"],data["bridge"]))
+        return "success!"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=4000, debug=True)
