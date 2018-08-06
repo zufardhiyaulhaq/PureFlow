2ee85bf7aead7bdde5d21eda3c881e7755486434
zufardhiyaulhaq <zufar@ieee.org>
2018-08-06 12:49:55 +0700

    testing

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,10 +9,13 @@ from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
 
+
 def get(append):
     url = 'http://127.0.0.1:8181/onos/v1'+append
-    data = requests.get(url, auth=HTTPBasicAuth('onos','rocks')).text.decode('utf-8')
+    data = requests.get(url, auth=HTTPBasicAuth(
+        'onos', 'rocks')).text.decode('utf-8')
     return json.loads(data)
+
 
 @app.route('/')
 def index():
@@ -21,10 +24,12 @@ def index():
     else:
         return render_template('index.html')
 
+
 @app.route('/logout')
 def logout():
     session['logged_in'] = False
     return redirect("/")
+
 
 @app.route('/login', methods=['POST'])
 def admin_login():
@@ -34,6 +39,7 @@ def admin_login():
         flash('wrong password!')
     return redirect("/")
 
+
 @app.route('/configuring')
 def configuring():
     if not session.get('logged_in'):
@@ -42,23 +48,127 @@ def configuring():
         data = get("/devices")
         return render_template('configuring.html', data=data)
 
-@app.route('/configuring/api', methods = ['POST','GET'])
+
+@app.route('/configuring/api', methods=['POST', 'GET'])
 def device():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
         raw = request.form.to_dict(flat=True)
         data = {k.encode('utf8'): v.encode('utf8') for k, v in raw.items()}
-        return render_template('configuring-device.html',data=data)
+        return render_template('configuring-device.html', data=data)
 
-@app.route('/configuring/controller', methods = ['POST'])
+
+@app.route('/configuring/controller', methods=['POST'])
 def configuring_api():
     raw = request.form.to_dict(flat=True)
     data = {k.encode('utf8'): v.encode('utf8') for k, v in raw.items()}
-    print (type(data))
-    print (data)
+    res = dict((k, v) for k, v in data.iteritems() if v is not None)
+    print (res)
+    print (type(res))
+    json = {
+        "priority": 40000,
+        "isPermanent": true,
+        "deviceId": None,
+        "treatment": {
+            "instructions": [
+                {
+                    "type": "OUTPUT",
+                    "port": None
+                },
+                {
+                    "type": "L2MODIFICATION",
+                    "subtype": "VLAN_ID",
+                    "vlanId": None
+                },
+                {
+                    "type": "L2MODIFICATION",
+                    "subtype": "ETH_SRC",
+                    "mac": None
+                }
+                {
+                    "type": "L2MODIFICATION",
+                    "subtype": "ETH_DST",
+                    "mac": None
+                },
+                {
+                    "type": "L3MODIFICATION",
+                    "subtype": "IPV4_SRC",
+                    "ip": None
+                },
+                {
+                    "type": "L3MODIFICATION",
+                    "subtype": "IPV4_DST",
+                    "ip": None
+                },
+                {
+                    "type": "L4MODIFICATION",
+                    "subtype": "TCP_SRC",
+                    "tcpPort": None
+                },
+                {
+                    "type": "L4MODIFICATION",
+                    "subtype": "UDP_SRC",
+                    "udpPort": None
+                }
+            ]
+        },
+        "selector": {
+            "criteria": [
+                {
+                    "type": "ETH_TYPE",
+                    "ethType": None
+                },
+                {
+                    "type": "ETH_DST",
+                    "mac": None
+                },
+                {
+                    "type": "ETH_SRC",
+                    "mac": None
+                },
+                {
+                    "type": "VLAN_VID",
+                    "vlanId": None
+                },
+                {
+                    "type": "IN_PORT",
+                    "port": None
+                },
+                {
+                    "type": "IP_PROTO",
+                    "protocol": None
+                },
+                {
+                    "type": "IPV4_SRC",
+                    "ip": None
+                },
+                {
+                    "type": "IPV4_DST",
+                    "ip": None
+                },
+                {
+                    "type": "TCP_SRC",
+                    "tcpPort": None
+                },
+                {
+                    "type": "TCP_DST",
+                    "tcpPort": None
+                },
+                {
+                    "type": "UDP_SRC",
+                    "udpPort": None
+                },
+                {
+                    "type": "UDP_DST",
+                    "udpPort": None
+                }
+            ]
+        }
+    }
 
     return ("success!")
+
 
 @app.route('/provisioning')
 def provisioning():
@@ -67,12 +177,14 @@ def provisioning():
     else:
         return render_template('provisioning.html')
 
+
 @app.route('/provisioning/openvswitch')
 def openvswitch():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
         return render_template('openvswitch.html')
+
 
 @app.route('/provisioning/mikrotik')
 def mikrotik():
@@ -81,27 +193,31 @@ def mikrotik():
     else:
         return render_template('mikrotik.html')
 
-@app.route('/provisioning/api', methods = ['POST'])
+
+@app.route('/provisioning/api', methods=['POST'])
 def openvswitch_api():
     raw = request.form.to_dict(flat=True)
     data = {k.encode('utf8'): v.encode('utf8') for k, v in raw.items()}
 
     if data["device-type"] == "openvswitch":
         os.system("export ANSIBLE_HOST_KEY_CHECKING=False")
-        os.system('ansible-playbook -u %s -i %s, /opt/PureFlow/ansible/playbook/openvswitch.yaml --extra-vars "controller=%s bridge=%s"'%(data["username"],data["device-ip"],data["controller"],data["bridge"]))
+        os.system('ansible-playbook -u %s -i %s, /opt/PureFlow/ansible/playbook/openvswitch.yaml --extra-vars "controller=%s bridge=%s"' %
+                  (data["username"], data["device-ip"], data["controller"], data["bridge"]))
         return "success!"
-    
+
     if data["device-type"] == "mikrotik":
         os.system("export ANSIBLE_HOST_KEY_CHECKING=False")
-        os.system("ansible all -i %s, -m raw -a '/openflow add name=%s controllers=%s; /openflow enable %s;  quit' -u %s --extra-vars 'ansible_password=%s ansible_port=%s'"%(data["device-ip"],data["bridge"],data["controller"],data["bridge"],data["username"],data["password"],data["port"]))
-        return "success!" 
-    
+        os.system("ansible all -i %s, -m raw -a '/openflow add name=%s controllers=%s; /openflow enable %s;  quit' -u %s --extra-vars 'ansible_password=%s ansible_port=%s'" %
+                  (data["device-ip"], data["bridge"], data["controller"], data["bridge"], data["username"], data["password"], data["port"]))
+        return "success!"
+
     if data["device-type"] == "mikrotik-port":
         os.system("export ANSIBLE_HOST_KEY_CHECKING=False")
-        os.system("ansible all -i %s, -m raw -a '/openflow port add switch=%s interface=%s disable=no;  quit' -u %s --extra-vars 'ansible_password=%s ansible_port=%s'"%(data["device-ip"],data["bridge"],data["interface"],data["username"],data["password"],data["port"]))
+        os.system("ansible all -i %s, -m raw -a '/openflow port add switch=%s interface=%s disable=no;  quit' -u %s --extra-vars 'ansible_password=%s ansible_port=%s'" %
+                  (data["device-ip"], data["bridge"], data["interface"], data["username"], data["password"], data["port"]))
         return "success!"
+
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     app.run(host="0.0.0.0", port=4000, debug=True)
-
