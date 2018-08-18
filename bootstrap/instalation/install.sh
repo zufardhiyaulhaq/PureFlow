511cbc68798277c6364308a71735863e13747c9c
Zufar Dhiyaulhaq <zufardhiyaulhaq@gmail.com>
2018-08-18 18:16:57 +0700

    modify install

--- bootstrap/instalation/install.sh
+++ bootstrap/instalation/install.sh
@@ -93,3 +93,35 @@ sudo systemctl enable onos.service
 sleep 10
 
 sudo systemctl start onos.service
+
+echo "Update the repository"
+echo "============================================"
+sudo apt-get -y update
+
+echo "Install mysql"
+echo "============================================"
+sudo apt-get -y install mysql-server libmysqlclient-dev
+
+echo "Bootstraping mysql database"
+echo "============================================"
+DATABASE="pureflow"
+USERDB="pureflowadmin"
+PASSWDDB="pureflowpassword"
+
+echo "Please enter root user MySQL password!"
+echo "============================================"
+read rootpasswd
+
+echo "Creating Databases"
+echo "============================================"
+mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${DATABASE};"
+mysql -uroot -p${rootpasswd} -e "CREATE USER '${USERDB}'@'%' IDENTIFIED BY '${PASSWDDB}';"
+mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${DATABASE} . * TO '${USERDB}'@'%';"
+mysql -uroot -p${rootpasswd} -e "SHOW GRANTS FOR '${USERDB}'@'%';"
+mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+
+echo "Import Databases"
+echo "============================================"
+sudo mysql -u${USERDB} -p${PASSWDDB} ${DATABASE} < /opt/PureFlow/bootstrap/instalation/pureflow.sql
+
+
